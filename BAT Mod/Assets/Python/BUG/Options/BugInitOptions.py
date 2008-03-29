dbc5fca97972442b17b4c12490c2954eb6f0c103
alerum <alerum@b164250a-9c37-0410-b80a-bd52c766b718>
2008-03-29 08:22:42 +0000

    had to revert to previous version.

---
+++ BAT Mod/Assets/Python/BUG/Options/BugInitOptions.py
@@ -0,0 +1,16 @@
+## BugAllOptions
+## Builds all of the option sets
+## BUG Mod - Copyright 2007
+
+# create the main options object
+import BugOptions
+
+# create each of the option sets
+import BugScreenSOptions
+import BugCityScreenOptions
+
+import BugNJAGCOptions
+import BugScoreOptions
+
+import BugAlertsOptions
+import BugAutologOptions
---
+++ BAT Mod/Assets/Python/CvGreatPersonModEventManager.py
@@ -0,0 +1,86 @@
+#
+# GreatPersonMod BTS
+# CvGreatPersonModEventManager
+# 
+
+from CvPythonExtensions import *
+
+import CvUtil
+import CvEventManager
+import sys
+import PyHelpers
+import CvGreatPersonScreen
+import RandomNameUtils
+import CvConfigParser
+# Change the value to enable or disable the features from the Great Person Mod.
+# Default value is true
+g_bGreatPersonModFeaturesEnabled = true
+
+gc = CyGlobalContext()
+
+PyPlayer = PyHelpers.PyPlayer
+
+g_iCount = 0     # autoincrement global used as the primary key for dictionary
+g_dict = dict()  # holds information transferred from here to CvGreatPersonScreen.py
+
+class CvGreatPersonModEventManager:
+
+	def __init__(self, eventManager):
+
+		GreatPerson(eventManager)
+
+class AbstractGreatPerson(object):
+
+	def __init__(self, eventManager, *args, **kwargs):
+		super(AbstractGreatPerson, self).__init__(*args, **kwargs)
+
+class GreatPerson(AbstractGreatPerson):
+
+	def __init__(self, eventManager, *args, **kwargs):
+		super(GreatPerson, self).__init__(eventManager, *args, **kwargs)
+
+		eventManager.addEventHandler("greatPersonBorn", self.onGreatPersonBorn)
+
+		self.eventMgr = eventManager
+
+		# Begin Test Code
+		# -- BAT Mod 0.08 – Test code removed to conform to The BUG Project 			# standards. --
+		# End Test Code
+
+
+	def onGreatPersonBorn(self, argsList):
+		'Great Person Born'
+
+		if(not g_bGreatPersonModFeaturesEnabled):
+			return
+		
+		pUnit, iPlayer, pCity = argsList
+		player = gc.getPlayer(iPlayer)
+        
+		# Check if we should even show the popup:
+		if pUnit.isNone() or pCity.isNone():
+			return
+		
+		#CvUtil.pyPrint("Great Person Born: Name:<%s> Player:<%s> City:<%s>"%(pUnit.getNameNoDesc(), player.getName(), pCity.getName()))
+		    
+		#If Person doesn't have unique name, give him a random one
+		if(len(pUnit.getNameNoDesc()) == 0):
+			iCivilizationType = player.getCivilizationType()
+			pUnit.setName(RandomNameUtils.getRandomCivilizationName(iCivilizationType))
+
+		#Show fancy lil popup if a human got the great person:
+		if player.isHuman():
+		
+			global g_dict
+			global g_iCount
+			
+			g_dict[g_iCount] = (pUnit, iPlayer, pCity)
+			
+			#CvUtil.pyPrint("Great Person Born 2: Name:<%s> iPlayer:<%d> City:<%s>"%(g_dict[g_iCount][0].getNameNoDesc(), g_dict[g_iCount][1], g_dict[g_iCount][2].getName()))
+			
+			popupInfo = CyPopupInfo()
+			popupInfo.setButtonPopupType(ButtonPopupTypes.BUTTONPOPUP_PYTHON_SCREEN)
+			popupInfo.setData1(g_iCount)
+			g_iCount += 1
+			popupInfo.setText(u"showGreatPersonScreen")
+			popupInfo.addPopup(iPlayer)
