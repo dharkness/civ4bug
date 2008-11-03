8db7aa408cb0cf68681d8f92ee356edaba2f2cc5
mrbighead <mrbighead@b164250a-9c37-0410-b80a-bd52c766b718>
2008-11-03 17:32:20 +0000

    - Added Changelog: please add a line if you add a new feature or fix a bug which may be interesting for the users to know. Also please check if I missed anything. - Redesigned General tab - Updated German translation

--- BUG Mod/CustomAssets/Python/BUG/Tabs/BugGeneralOptionsTab.py
+++ BUG Mod/CustomAssets/Python/BUG/Tabs/BugGeneralOptionsTab.py
@@ -19,40 +19,49 @@ class BugGeneralOptionsTab(BugOptionsTab.BugOptionsTab):
 		tab = self.createTab(screen)
 		panel = self.createMainPanel(screen)
 		column = self.addOneColumnLayout(screen, panel)
-
-		left, center, right = self.addThreeColumnLayout(screen, column, "Top", True)
-
+		
+		left, space1, center, space2, right = self.addMultiColumnLayout(screen, column, 5, "Top", False)
+		
 		self.addLabel(screen, left, "MainInterface", "MAIN INTERFACE")
 		self.addCheckbox(screen, left, "MainInterface__GPBar")
 		self.addTextDropdown(screen, left, left, "MainInterface__GPBar_Types", True)
-
-		self.addSpacer(screen, left, "MainInterface_TechWindow")
-		self.addLabel(screen, left, "TechWindow", "Tech Splash Screen:")
-		self.addTextDropdown(screen, left, left, "TechWindow__ViewType", True)
-		self.addCheckbox(screen, left, "TechWindow__CivilopediaText")
-
-		self.addSpacer(screen, left, "Misc")
-		self.addLabel(screen, left, "Misc", "Misc:")
-		self.addCheckbox(screen, left, "MainInterface__Combat_Counter")
-		self.addCheckbox(screen, left, "MainInterface__CityArrows")
+		self.addSpacer(screen, left, "MainInterface_FieldOfView")
 		self.addCheckbox(screen, left, "MainInterface__FieldOfView")
 		self.addCheckbox(screen, left, "MainInterface__FieldOfView_Remember")
-
-		self.addLabel(screen, center, "CityScreen", "CITY SCREEN")
-		self.addCheckbox(screen, center, "CityScreen__RawYields")
-		self.addTextDropdown(screen, center, center, "CityScreen__RawYields_View", True)
-
-		self.addSpacer(screen, center, "CityScreen_TopCenter")
-		self.addCheckbox(screen, center, "CityScreen__WhipAssist")
-		self.addCheckbox(screen, center, "CityScreen__WhipAssistOverflowCountCurrentProduction")
-		self.addCheckbox(screen, center, "CityScreen__Anger_Counter")
-
-		self.addSpacer(screen, center, "CityScreen_Bottom")
-		self.addLabel(screen, center, "Misc", "Misc:")
-		self.addCheckbox(screen, center, "CityScreen__CultureTurns")
-		self.addCheckbox(screen, center, "CityScreen__GreatPersonTurns")
-		self.addCheckbox(screen, center, "CityScreen__GreatPersonInfo")
-		self.addTextDropdown(screen, center, center, "CityScreen__Specialists", True)
-
+		
+		self.addSpacer(screen, space1, "Main_TechWindow", 3)
+		
+		self.addSpacer(screen, center, "MainInterface_TechWindow")
+		self.addLabel(screen, center, "TechWindow", "Tech Splash Screen:")
+		self.addTextDropdown(screen, center, center, "TechWindow__ViewType", True)
+		self.addCheckbox(screen, center, "TechWindow__CivilopediaText")
+		
+		self.addSpacer(screen, space2, "Main_Misc", 3)
+		
+		self.addSpacer(screen, right, "Misc")		
+		self.addLabel(screen, right, "Misc", "Misc:")
+		self.addCheckbox(screen, right, "MainInterface__Combat_Counter")
+		self.addCheckbox(screen, right, "MainInterface__CityArrows")
+		
+		screen.attachHSeparator(column, column + "Sep")
+		
+		left, space, right = self.addThreeColumnLayout(screen, column, "Bottom", False)
+		
+		self.addLabel(screen, left, "CityScreen", "CITY SCREEN")
+		self.addCheckbox(screen, left, "CityScreen__RawYields")
+		self.addTextDropdown(screen, left, left, "CityScreen__RawYields_View", True)
+		
+		self.addSpacer(screen, left, "CityScreen_TopCenter")
+		self.addCheckbox(screen, left, "CityScreen__WhipAssist")
+		self.addCheckbox(screen, left, "CityScreen__WhipAssistOverflowCountCurrentProduction")
+		self.addCheckbox(screen, left, "CityScreen__Anger_Counter")
+		
+		self.addSpacer(screen, space, "City_Misc", 3)
+		
+		self.addSpacer(screen, right, "CityScreen_Bottom")
+		self.addLabel(screen, right, "Misc", "Misc:")
+		self.addCheckbox(screen, right, "CityScreen__CultureTurns")
+		self.addCheckbox(screen, right, "CityScreen__GreatPersonTurns")
+		self.addCheckbox(screen, right, "CityScreen__GreatPersonInfo")
 		self.addCheckbox(screen, right, "MainInterface__ProgressBarsTickMarks")
-
+		self.addTextDropdown(screen, right, right, "CityScreen__Specialists", True)		
