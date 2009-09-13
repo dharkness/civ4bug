a9eaf16acce52d18972217c1378013453fdeeddb
emperorfool <emperorfool@b164250a-9c37-0410-b80a-bd52c766b718>
2009-09-13 22:06:05 +0000

    Fix: Autolog - File name and path changes take effect immediately

--- trunk/CustomAssets/Python/Contrib/autolog.py
+++ trunk/CustomAssets/Python/Contrib/autolog.py
@@ -4,80 +4,119 @@
 ## contains variables to turn on and off various extra log messages
 ## Alt+E is always on
 
-import codecs
-import os
-import os.path
-import string
+from CvPythonExtensions import *
+import BugConfigTracker
 import BugCore
 import BugOptions
 import BugPath
-import BugConfigTracker
+import BugUtil
+import CvModName
+import codecs
+import os.path
+import time
 
-BugAutolog = BugCore.game.Autolog
+gc = CyGlobalContext()
+AutologOpt = BugCore.game.Autolog
 
 class autologInstance:
 
-
 	def __init__(self):
 		self.MsgStore = []
-		self.LogFileName = BugAutolog.getFileName()
+		self.bStarted = False
+		self.LogFileName = None
+		self.LogFilePath = None
+		self.RealLogFile = None
+		self.setLogFileName(AutologOpt.getFileName())
+		self.setLogFilePath(AutologOpt.getFilePath())
 
 	def setLogFileName(self, LogFileName, bSaveToOptions=False):
 		if (bSaveToOptions):
-			BugAutolog.setFileName(LogFileName)
-			# TODO: do we need to save this?
+			AutologOpt.setFileName(LogFileName)
 			BugOptions.write()
 		self.LogFileName = LogFileName
+		self.updateLogFile()
 		
 	def getLogFileName(self):
 		return self.LogFileName
+	
+	def setLogFilePath(self, LogFilePath, bSaveToOptions=False):
+		if (bSaveToOptions):
+			AutologOpt.setFilePath(LogFilePath)
+			BugOptions.write()
+		if (not LogFilePath or LogFilePath == "Default"):
+			LogFilePath = BugPath.findOrMakeDir("Autolog")
+		self.LogFilePath = LogFilePath
+		self.updateLogFile()
+		
+	def getLogFilePath(self):
+		return self.LogFilePath
+	
+	def updateLogFile(self):
+		if self.LogFileName and self.LogFilePath:
+			self.bStarted = False
+			self.RealLogFile = os.path.join(self.LogFilePath, self.LogFileName)
+			BugConfigTracker.add("Autolog_Log", self.RealLogFile)
 
 	def isLogging(self):
-		return BugAutolog.isLoggingOn()
+		return AutologOpt.isLoggingOn()
+	
+	def start(self):
+		self.writeMsg("")
+		self.writeMsg("Logging by " + CvModName.getDisplayNameAndVersion() + " (" + CvModName.getCivNameAndVersion() + ")")
+		self.writeMsg("------------------------------------------------")
+		zcurrturn = gc.getGame().getElapsedGameTurns() + AutologOpt.get4000BCTurn()
+		zmaxturn = gc.getGame().getMaxTurns()
+		zyear = gc.getGame().getGameTurnYear()
+		if (zyear < 0):
+			zyear = str(-zyear) + BugUtil.getPlainText("TXT_KEY_AUTOLOG_BC")
+		else:
+			zyear = str(zyear) + BugUtil.getPlainText("TXT_KEY_AUTOLOG_AD")
+		zCurrDateTime = time.strftime("%d-%b-%Y %H:%M:%S")
+		if (zmaxturn == 0):
+			zsTurn = "%i" % (zcurrturn)
+		else:
+			zsTurn = "%i/%i" % (zcurrturn, zmaxturn)
+		message = BugUtil.getText("TXT_KEY_AUTOLOG_TURN", (zsTurn, zyear, zCurrDateTime))
+		self.writeMsg(message, vBold=True, vUnderline=True)
+		self.bStarted = True
 
 	def writeLog(self, vMsg, vColor = "Black", vBold = False, vUnderline = False, vPrefix = ""):
-
-		if len(self.MsgStore) > 0:
-			self.openLog()
-			for sMsg in self.MsgStore:
-				self.log.write(sMsg)
-			self.closeLog()
-			self.writeLog_pending_flush()
-
-		zMsg = self.buildMsg(vMsg, vColor, vBold, vUnderline, vPrefix)
-
 		self.openLog()
-		self.log.write(zMsg)
+		if len(self.MsgStore) > 0:
+			msgs = self.MsgStore
+			self.writeLog_pending_flush()
+			for sMsg in msgs:
+				self.log.write(sMsg)
+		self.writeMsg(vMsg, vColor, vBold, vUnderline, vPrefix)
 		self.closeLog()
+
+	def writeMsg(self, vMsg, vColor = "Black", vBold = False, vUnderline = False, vPrefix = ""):
+		zMsg = self.buildMsg(vMsg, vColor, vBold, vUnderline, vPrefix)
+		self.log.write(zMsg)
 
 	def writeLog_pending(self, vMsg, vColor = "Black", vBold = False, vUnderline = False, vPrefix = ""):
 		zMsg = self.buildMsg(vMsg, vColor, vBold, vUnderline, vPrefix)
-		self.MsgStore.append (zMsg)
+		self.MsgStore.append(zMsg)
 
 	def writeLog_pending_flush(self):
 		self.MsgStore = []
 
 	def openLog(self):
-		szPath = BugAutolog.getFilePath()
-		if (not szPath or szPath == "Default"):
-			szPath = BugPath.findOrMakeDir("Autolog")
-		if (not os.path.isdir(szPath)):
-			os.makedirs(szPath)
-		szFile = os.path.join(szPath, self.LogFileName)
-		self.log = codecs.open(szFile, 'a', 'utf-8')
-		BugConfigTracker.add("Autolog_Log", szFile)
+		self.log = codecs.open(self.RealLogFile, 'a', 'utf-8')
+		if not self.bStarted:
+			self.start()
 
 	def closeLog(self):
 		self.log.close()
 
 	def buildMsg(self, vMsg, vColor, vBold, vUnderline, vPrefix):
-		if vPrefix != "":
-			zMsg = "%s %s" % (vPrefix, vMsg)
+		if vPrefix:
+			zMsg = vPrefix + " " + vMsg
 		else:
 			zMsg = vMsg
 
 		## determine type of message
-		zStyle = BugAutolog.getFormatStyle()
+		zStyle = AutologOpt.getFormatStyle()
 		if (zStyle < 0
 		or zStyle > 3): zStyle=0
 
@@ -90,7 +129,7 @@ class autologInstance:
 			if vUnderline:
 				zMsg = "<u>%s</u>" % (zMsg)
 			if (vColor != "Black"
-			and BugAutolog.isColorCoding()):
+			and AutologOpt.isColorCoding()):
 				zMsg = "<span style=\"color: %s\">%s</span>" % (vColor, zMsg)
 
 			zMsg = "%s<br>" % (zMsg)
@@ -101,18 +140,10 @@ class autologInstance:
 			if vUnderline:
 				zMsg = "[u]%s[/u]" % (zMsg)
 			if (vColor != "Black"
-			and BugAutolog.isColorCoding()):
+			and AutologOpt.isColorCoding()):
 				if zStyle == 2:  # color coding with "
 					zMsg = "[color=\"%s\"]%s[/color]" % (vColor, zMsg)
 				else:  # color coding without "
 					zMsg = "[color=%s]%s[/color]" % (vColor, zMsg)
 
 		return "%s\r\n" % (zMsg)
-
-# EF: What is this unused class for?
-class autologRetain:
-
-	def __init__(self):
-		bLogFileOpen = False
-		bPlayerHuman = False
-		Counter = 0
