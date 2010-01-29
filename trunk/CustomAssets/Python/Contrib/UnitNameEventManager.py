91d3a94630db2a5ed60e9893c371834220207326
emperorfool <emperorfool@b164250a-9c37-0410-b80a-bd52c766b718>
2010-01-29 07:40:01 +0000

    Changed "X-Ray" to "Xray" to match NATO standard.

--- trunk/CustomAssets/Python/Contrib/UnitNameEventManager.py
+++ trunk/CustomAssets/Python/Contrib/UnitNameEventManager.py
@@ -104,7 +104,7 @@ PyInfo = PyHelpers.PyInfo
 UnitNamingOpt = BugCore.game.UnitNaming
 
 phonetic_array = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliet', 'Kilo', 'Lima', 'Mike',
-                  'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-Ray', 'Yankee', 'Zulu']
+                  'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'Xray', 'Yankee', 'Zulu']
 
 greek_array = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi',
                'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega']
