5ad9feae031f045b5ffe0e12f049c3a91aa0a54b
David Harkness <341319+dharkness@users.noreply.github.com>
2010-05-09 18:48:09 +0000

    Foreign Advisor INFO tab shows correct modifier total, including visible values from Show Hidden Attitude mod if active.

--- CustomAssets/Python/Screens/CvExoticForeignAdvisor.py
+++ CustomAssets/Python/Screens/CvExoticForeignAdvisor.py
@@ -585,6 +585,7 @@ class CvExoticForeignAdvisor (CvForeignAdvisor.CvForeignAdvisor):
 			
 			objLeaderHead = gc.getLeaderHeadInfo (objLoopPlayer.getLeaderType())
 			objAttitude = AttitudeUtil.Attitude(iLoopPlayer, self.iActiveLeader)
+			AttitudeUtil.getAttitudeText(j, iLoopPlayer, AdvisorOpt.isShowGlanceNumbers(), AdvisorOpt.isShowGlanceSmilies(), True, True)
 
 			# Player panel
 			playerPanelName = self.getNextWidgetName()
